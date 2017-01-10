1055b2ae571fba787b1a03b485c96a0f8ad4b5aa
heliang5241 <1960050004@qq.com>
2017-01-10 16:33:12 +0800

    change

--- budayu6.sh
+++ budayu6.sh
@@ -3,7 +3,16 @@ a="print the length of the longest line"
 for i in $a
 do
 num=`echo $i|wc -L`
-if [ $num -le 2 ];then
+if [ $num -le 4 ];then
 echo $i
+fi
+done
+
+echo "########"
+for i in $a
+do
+if [ ${#i} -le 4 ];then
+  echo $i
+
 fi
 done
