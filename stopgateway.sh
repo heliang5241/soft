bd82c6e1353da628767e60334f492457f93eb2fb
heliang5241 <1960050004@qq.com>
2017-01-16 08:05:02 +0800

    0116

--- stopgateway.sh
+++ stopgateway.sh
@@ -1,26 +1,44 @@
 #!/bin/bash
-#first=`cat /etc/sysconfig/network-scripts/ifcfg-eth0 |awk '{if(NR==11)print $0;}'|cut -c 1`
 hang=`cat /etc/sysconfig/network-scripts/ifcfg-*|grep GATEWAY -n|awk -F ':' '{print $1}'`
+first=`cat /etc/sysconfig/network-scripts/ifcfg-eth0 |awk '{if(NR=='$hang')print $0;}'|cut -c 1`
 message(){
         echo "Usage $0 start|stop"
        
 }
 
 stop_gateway(){
-sed -i ''$hang's/^/#/' /etc/sysconfig/network-scripts/ifcfg-eth0
-}
-
-start_gateway(){
-first=`cat /etc/sysconfig/network-scripts/ifcfg-eth0 |awk '{if(NR==11)print $0;}'|cut -c 1`
-while [ $first = '#' ]
+if [ $first = '#' ];then
+  echo -e "\033[31mGATEWAY HAS BEEN STOPED.DO NOTHING! \033[0m"
+  exit;
+fi
+while [ $first != '#' ]
 do
-   sed -i ''$hang's/^#//' /etc/sysconfig/network-scripts/ifcfg-eth0
-if [ $first != '#' ];then
+   sed -i ''$hang's/^/#/' /etc/sysconfig/network-scripts/ifcfg-eth0
+   hh=`cat /etc/sysconfig/network-scripts/ifcfg-eth0 |awk '{if(NR=='$hang')print $0;}'|cut -c 1`
+if [ $hh = '#' ];then
    break;
 fi
 done
+/sbin/service network restart
+echo -e "\033[31mGATEWAY IS STOPING \033[0m"
 }
 
+start_gateway(){
+if [ $first = 'G' ];then
+  echo -e "\033[31mGATEWAY HAS BEEN STARTED.DO NOTHING! \033[0m"
+  exit;
+fi
+while [ $first = '#' ]
+do
+   sed -i ''$hang's/^#//' /etc/sysconfig/network-scripts/ifcfg-eth0
+   hh=`cat /etc/sysconfig/network-scripts/ifcfg-eth0 |awk '{if(NR=='$hang')print $0;}'|cut -c 1`
+if [ $hh = 'G' ];then
+   break;
+fi
+done
+/sbin/service network restart
+echo -e "\033[31mGATEWAY IS STARTING \033[0m"
+}
 case $1 in
 stop)
     stop_gateway
